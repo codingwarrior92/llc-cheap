5025ba6e9f8cf871ded46efa1ccd0a5f458bf018
Chris Rodriguez <christopher.eugene.rodriguez@gmail.com>
2023-01-09 01:05:50 -0800

    auth pages

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// COMPONENTS
+import { ForgotPasswordComponent } from './modules/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent }
+
 ];
 
 @NgModule({
